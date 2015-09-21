40c08634acac5d2df71acbf6c9fbbff774929b8d
Claus Gaarde Pedersen <clausgaardepedersen@gmail.com>
2015-09-21 13:41:52 +0200

    worked on cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## makeCacheMatrix defines a list of 4 functions to be
+## used on a matrix to help create its inverted matrix
+## and cache both the matrix and its inverted matrix
+## To be used in conjunction with cacheSolve
+## set        : change the cached matrix
+## get        : returns the cached matrix
+## setimatrix : stores the 'inverse' matrix
+## getimatrix : returns the stored 'inverse' matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setimatrix <- function(imatrix) m <<- imatrix
+        getimatrix <- function() m
+        list(set = set, get = get,
+             setimatrix = setimatrix,
+             getimatrix = getimatrix)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve returns an inverse matrix
+## Has to be used in conjunction with makeCacheMatrix
+## to create the helping functions first
+## example use : imatrlist <- makeCacheMatrix(some matrix)
+##               cacheSolve(imatrlist)
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getimatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setimatrix(m)
+        m
 }
